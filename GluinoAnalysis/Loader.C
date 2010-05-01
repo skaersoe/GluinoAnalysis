0f37230459a8122583d1611d3b5010f8bca0394e
Morten Dam Joergensen <mdj@damconsult.dk>
2010-05-01 23:50:19 +0200

    rem

--- GluinoAnalysis/Loader.C
+++
@@ -1,9 +0,0 @@
-#include <vector> 
-#ifdef __CINT__ 
-#pragma link C++ class vector<vector<float> >; 
-#pragma link C++ class vector<vector<int> >; 
-#pragma link C++ class pair<string,string>;
-#else 
-template class std::vector<std::vector<float> >; 
-template class std::vector<std::vector<int> >; 
-#endif
